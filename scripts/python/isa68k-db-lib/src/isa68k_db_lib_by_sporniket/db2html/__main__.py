e9833465f98d1c2ef2c361fded7ab15a83756df3
David SPORN <sporniket.studio@gmail.com>
2025-10-19 06:49:29 +0200

    [tech] add script to call db2html locally

---
+++ scripts/python/isa68k-db-lib/src/isa68k_db_lib_by_sporniket/db2html/__main__.py
@@ -0,0 +1,11 @@
+# GPL3 or later
+
+
+def main():
+    print(
+        "db2html, part of the python library for the isa68k-database project, by Sporniket"
+    )
+
+
+if __name__ == "__main__":
+    main()
